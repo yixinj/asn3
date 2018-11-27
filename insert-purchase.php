1f1d6c47599e1082a3b18a99383a578f9c22c72e
Yixin Jiang <yixin77@gmail.com>
2018-11-27 14:58:05 -0500

    autocommit

--- insert-purchase.php
+++ insert-purchase.php
@@ -1,6 +1,99 @@
-<!--
-      Insert a new purchase (prompt for necessary data) Note: Send an error message if they try to give an invalid customer id number or invalid product number (or make your GUI so that it doesnt allow them to pick those). If the user tries to let a customer purchase a product they already have purchased, instead just let them change the quantity that the customer will have purchased of that product.  Don't allow the quantity to go lower, just higher by the amount they want now.
-    -->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Yixin | Other</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <script src="main.js"></script>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous" />
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">CS3319 Assignment 3</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="customers.php">Customers <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="products.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="other.php">Other</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Connect to DB -->
+  <?php include 'connect-db.php';?>
+
+  <!--
+        TODO: Insert a new purchase (prompt for necessary data) Note: Send an error message if they try to give an invalid customer id number or invalid product number (or make your GUI so that it doesnt allow them to pick those). If the user tries to let a customer purchase a product they already have purchased, instead just let them change the quantity that the customer will have purchased of that product.  Don't allow the quantity to go lower, just higher by the amount they want now.
+      -->
+  <div class="container">
+    <h1 class="section-title">Other</h1>
+    <div class="card">
+      <div class="card-body">
+        <h3 class="card-title">Insert Purchase</h3>
+        <p class="card-text">
+        <?php
+            // Retrieves form submission
+            $customer_id =  $_POST["customer_id"];
+            $product_id = $_POST["product_id"];
+            $quantity =$_POST["quantity"];
+
+            // Inserts into table
+            $query = 'INSERT INTO purchases values("' . $customer_id . '","' . $product_id . '","' . $quantity . '")';
+            if (!mysqli_query($connection, $query)) {
+                die("Error - insert failed: " . mysqli_error($connection));
+            }
+            echo "Your purchase was added!";
+            mysqli_close($connection);
+        ?>
+        </p>
+        <!-- Back button :) -->
+        <a href="other.php" class="btn btn-secondary">View other</a>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted">CS3319 Assignment 3 by Yixin Jiang</span>
+    </div>
+  </footer>
+
+
+  <!-- Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
 <?php
 // Results from post (clicking submit)
    $customer_id =  $_POST["customer_id"];
