ffafa9fe5158eadf477a24c61570e615dfb6aec6
Yixin Jiang <yixin77@gmail.com>
2018-11-26 20:53:27 -0500

    autocommit

--- index.php
+++ index.php
@@ -27,13 +27,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Customers</a>
+          <a class="nav-link" href="customers.php">Customers</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
+          <a class="nav-link" href="products.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="other.php">Other</a>
         </li>
       </ul>
     </div>
@@ -68,7 +71,7 @@
     </div>
     <div class="card">
       <div class="card-header">
-        Purchases
+        Other
       </div>
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
